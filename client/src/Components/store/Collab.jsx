2ab6059753a273d81dd0ecfed8afbe122982205b
Sourabh Sikarwar <sourabhsikarwar1812@gmail.com>
2023-02-12 15:07:26 +0530

    e-commerce added, cycle complete

--- client/src/Components/store/Collab.jsx
+++ client/src/Components/store/Collab.jsx
@@ -5,7 +5,7 @@ const Collab = () => {
   return (
     <div className="container max-w-7xl mx-auto my-4 p-4">
         <h1 className='text-center mx-auto text-2xl text-darkGreen font-semibold my-4'>Our Collaborator for shopping and coupons</h1>
-      <div className="h-84 w-full p-8 rounded-xl grid grid-cols-3 justify-center items-center gap-8 bg-white">
+      <div className="h-84 w-full p-8 rounded-xl grid grid-cols-1 md:grid-cols-3 justify-center items-center gap-8 bg-white">
         <div className='cursor-pointer'>
             <img src="https://res.cloudinary.com/dzdaulvbt/image/upload/v1676184047/Products/collab3_kgfrsm.png" className='mx-auto w-1/3' alt="collab" />
         </div>
